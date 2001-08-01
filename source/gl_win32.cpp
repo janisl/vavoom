397a72f23917b65ce187d865e6a6b2e736226cbd
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2001-08-01 17:43:51 +0000

    Some modeset improvements

--- source/gl_win32.cpp
+++ source/gl_win32.cpp
@@ -70,6 +70,9 @@ void TOpenGLDrawer::Init(void)
 
 bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 {
+	int			pixelformat;
+	MSG			msg;
+
 	if (!Width || !Height)
 	{
 		//	Set defaults
@@ -86,10 +89,10 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		return false;
 	}
 
-	int pixelformat;
-
+	//	Sut down current mode
 	Shutdown();
 
+	//	Try to switch to the new mode
 	DEVMODE dmScreenSettings;
 	memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
 	dmScreenSettings.dmSize = sizeof(dmScreenSettings);
@@ -102,8 +105,28 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 	{
 		return false;
 	}
+
+	//	Now we try to make sure we get the focus on the mode switch, because
+	// sometimes in some systems we don't. We grab the foreground, pump all
+	// our messages, and sleep for a little while to let messages finish
+	// bouncing around the system, then we put ourselves at the top of the z
+	// order, then grab the foreground again.
+	//	Who knows if it helps, but it probably doesn't hurt
+	SetForegroundWindow(hwnd);
+
+	while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+	{
+      	TranslateMessage(&msg);
+      	DispatchMessage(&msg);
+	}
+
+	Sleep(100);
+
 	SetWindowPos(hwnd, HWND_TOP, 0, 0, Width, Height, SWP_NOMOVE);
 
+	SetForegroundWindow(hwnd);
+
+	//	Get device context
 	DeviceContext = GetDC(hwnd);
 	if (!DeviceContext)
 	{
@@ -111,6 +134,13 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		return false;
 	}
 
+	//	Because we have set the background brush for the window to NULL
+	// (to avoid flickering when re-sizing the window on the desktop), we
+	// clear the window to black when created, otherwise it will be
+	// empty while Vavoom starts up.
+	PatBlt(DeviceContext, 0, 0, Width, Height, BLACKNESS);
+
+	//	Set up pixel format
 	PIXELFORMATDESCRIPTOR pfd =	// pfd Tells Windows How We Want Things To Be
 	{
 		sizeof(PIXELFORMATDESCRIPTOR),	// Size Of This Pixel Format Descriptor
@@ -125,7 +155,7 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		0,								// Shift Bit Ignored
 		0,								// No Accumulation Buffer
 		0, 0, 0, 0,						// Accumulation Bits Ignored
-		16,								// 16Bit Z-Buffer (Depth Buffer)
+		32,								// 32Bit Z-Buffer (Depth Buffer)
 		0,								// No Stencil Buffer
 		0,								// No Auxiliary Buffer
 		PFD_MAIN_PLANE,					// Main Drawing Layer
@@ -143,6 +173,7 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		Sys_Error("SetPixelFormat failed");
 	}
 
+	//	Create rendering context
 	RenderContext = wglCreateContext(DeviceContext);
 	if (!RenderContext)
 	{
@@ -150,6 +181,7 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		return false;
 	}
 
+	//	Make this context current
 	if (!wglMakeCurrent(DeviceContext, RenderContext))
 	{
 		char* lpMsgBuf;
@@ -167,6 +199,7 @@ bool TOpenGLDrawer::SetResolution(int Width, int Height, int BPP)
 		return false;
 	}
 
+	//	Everything is fine, set some globals and finish
 	ScreenWidth = Width;
 	ScreenHeight = Height;
 	ScreenBPP = BPP;
@@ -218,9 +251,12 @@ void TOpenGLDrawer::Shutdown(void)
 //**************************************************************************
 //
 //	$Log$
+//	Revision 1.4  2001/08/01 17:43:51  dj_jl
+//	Some modeset improvements
+//
 //	Revision 1.3  2001/07/31 17:16:30  dj_jl
 //	Just moved Log to the end of file
-//
+//	
 //	Revision 1.2  2001/07/27 14:27:54  dj_jl
 //	Update with Id-s and Log-s, some fixes
 //
