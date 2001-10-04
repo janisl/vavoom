e6ea6a42daedaf77cb1cf7c9d20a881453f62c95
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2001-10-04 17:21:06 +0000

    Cheating as cvar

--- source/automap.h
+++ source/automap.h
@@ -42,14 +42,16 @@ void AM_Stop(void);
 // PUBLIC DATA DECLARATIONS ------------------------------------------------
 
 extern boolean 		automapactive;	// In AutoMap mode?
-extern int 			am_cheating;
 
 //**************************************************************************
 //
 //	$Log$
+//	Revision 1.4  2001/10/04 17:21:06  dj_jl
+//	Cheating as cvar
+//
 //	Revision 1.3  2001/07/31 17:16:30  dj_jl
 //	Just moved Log to the end of file
-//
+//	
 //	Revision 1.2  2001/07/27 14:27:54  dj_jl
 //	Update with Id-s and Log-s, some fixes
 //
