00d44ec0cb8c997c7c4080a8474d98bf5e88e7c5
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2002-01-28 18:41:16 +0000

    Fixed response files

--- source/misc.cpp
+++ source/misc.cpp
@@ -86,73 +86,65 @@ void M_InitArgs(int argc, char **argv)
 #ifndef DJGPP
 static void M_FindResponseFile(void)
 {
-    int         i;
-	
-    for (i = 1;i < myargc;i++)
-	  if (myargv[i][0] == '@')
-	  {
-	    FILE *  handle;
-	    int     size;
-	    int     k;
-	    int     index;
-	    int    	indexinfile;
-	    char    *infile;
-	    char    *file;
-	    char    *moreargs[20];
-	    char    *firstargv;
-			
-	    // READ THE RESPONSE FILE INTO MEMORY
-	    handle = fopen(&myargv[i][1], "rb");
-	    if (!handle)
-	    {
+	for (int i = 1;i < myargc;i++)
+	{
+		if (myargv[i][0] != '@')
+		{
+			continue;
+		}
+
+		//	Read the response file into memory
+		FILE *handle = fopen(&myargv[i][1], "rb");
+		if (!handle)
+		{
 			printf("\nNo such response file %s!", &myargv[i][1]);
 			exit(1);
-	    }
-	    dprintf("Found response file %s!\n", &myargv[i][1]);
-	    fseek(handle, 0, SEEK_END);
-	    size = ftell(handle);
-	    fseek(handle, 0, SEEK_SET);
-	    file = (char*)malloc(size);
-	    fread(file, size, 1, handle);
-	    fclose(handle);
-			
-	    // KEEP ALL CMDLINE ARGS FOLLOWING @RESPONSEFILE ARG
-	    for (index = 0,k = i+1; k < myargc; k++)
-			moreargs[index++] = myargv[k];
-			
-	    firstargv = myargv[0];
-	    myargv = (char**)malloc(sizeof(char *) * MAXARGVS);
-	    memset(myargv, 0, sizeof(char *) * MAXARGVS);
-	    myargv[0] = firstargv;
-			
-	    infile = file;
-	    indexinfile = k = 0;
-	    indexinfile++;  // SKIP PAST ARGV[0] (KEEP IT)
-	    do
-	    {
-			myargv[indexinfile++] = infile+k;
-			while(k < size &&
-		      ((*(infile+k)>= ' '+1) && (*(infile+k)<='z')))
-		    k++;
-			*(infile+k) = 0;
-			while(k < size &&
-		      ((*(infile+k)<= ' ') || (*(infile+k)>'z')))
-		    k++;
-	    } while(k < size);
-			
-	    for (k = 0;k < index;k++)
-			myargv[indexinfile++] = moreargs[k];
-	    myargc = indexinfile;
-	
-	    // DISPLAY ARGS
-		if (M_CheckParm("-debug"))
-		{
-		    dprintf("%d command-line args:\n",myargc);
-		    for (k=1;k<myargc;k++)
-				dprintf("%s\n",myargv[k]);
 		}
-	    break;
-	  }
+		dprintf("Found response file %s!\n", &myargv[i][1]);
+		fseek(handle, 0, SEEK_END);
+		int size = ftell(handle);
+		fseek(handle, 0, SEEK_SET);
+		char *file = (char*)malloc(size);
+		fread(file, size, 1, handle);
+		fclose(handle);
+			
+		//	Keep all other cmdline args
+		char **oldargv = myargv;
+			
+		myargv = (char**)malloc(sizeof(char *) * MAXARGVS);
+		memset(myargv, 0, sizeof(char *) * MAXARGVS);
+		
+		//	Keep args before response file
+		int indexinfile;
+		for (indexinfile = 0; indexinfile < i; indexinfile++)
+		{
+			myargv[indexinfile] = oldargv[indexinfile];
+		}
+
+		//	Read response file
+		char *infile = file;
+		int k = 0;
+		do
+		{
+			myargv[indexinfile++] = infile + k;
+			while (k < size && ((*(infile + k) > ' ') && (*(infile + k) <= 'z')))
+				k++;
+			*(infile + k) = 0;
+			while (k < size && ((*(infile + k) <= ' ') || (*(infile + k) > 'z')))
+				k++;
+		} while (k < size);
+
+		//	Keep args following response file
+		for (k = i + 1; k < myargc; k++)
+			myargv[indexinfile++] = oldargv[k];
+		myargc = indexinfile;
+	
+		//	Display args
+	    dprintf("%d command-line args:\n", myargc);
+		for (k = 1; k < myargc; k++)
+			dprintf("%s\n", myargv[k]);
+		i--;
+	}
 }
 #endif
 
@@ -390,9 +382,12 @@ int PassFloat(float f)
 //**************************************************************************
 //
 //	$Log$
+//	Revision 1.9  2002/01/28 18:41:16  dj_jl
+//	Fixed response files
+//
 //	Revision 1.8  2002/01/07 12:16:42  dj_jl
 //	Changed copyright year
-//
+//	
 //	Revision 1.7  2001/10/08 17:33:40  dj_jl
 //	Four va buffers
 //	
