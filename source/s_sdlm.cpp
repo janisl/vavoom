2abbf7ab688b11d41708ba33a0027a22fadaff18
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2005-05-26 17:00:46 +0000

    Working MUS to MID conversion

--- source/s_sdlm.cpp
+++ source/s_sdlm.cpp
@@ -101,7 +101,11 @@ struct Track
 
 static void			StartMidiSong(char* song, boolean loop);
 static void			StopMidiSong(void);
+#if 0
 static boolean			LoadMUS(int len);
+#else
+int qmus2mid(char *mus, char *mid, int length);
+#endif
 static void			FreeLump(void);
 static void			FreeTracks(void);
 static int			write32bit(int handle, dword data);
@@ -122,6 +126,8 @@ static boolean		mus_looping = false;
 static void*		Mus_SndPtr  = NULL;
 static boolean		MusicPaused = false;
 static int			MusVolume = -1;
+
+static char			*mid_file;
 
 static struct Track	tracks[MIDI_TRACKS];
 static word			TrackCnt = 0;
@@ -353,8 +359,43 @@ static void StartMidiSong(char* song, boolean loop)
 	}
 	else if (!memcmp(Mus_SndPtr, MUSMAGIC, 4))
 	{
-    		res = LoadMUS(len);
+#if 0
+		res = LoadMUS(len);
 		if (!res) return;
+
+		if ((handle = Sys_FileOpenWrite("vv_temp.mid")) < 0) return;
+
+		memcpy(hdr.ID, MIDIMAGIC, 4);
+		hdr.hdr_size = BigLong(6);
+		hdr.num_tracks = BigShort((word)TrackCnt);
+		hdr.divisions = BigShort((word)mididata.divisions);
+		if (TrackCnt > 1) hdr.type = BigShort(1);
+		else hdr.type = 0;
+
+		if (Sys_FileWrite(handle, &hdr, sizeof(hdr)) != sizeof(hdr)) return;
+
+		for (i = 0; i < TrackCnt; i++)
+		{
+			dword tlen = mididata.track[i].len;
+			if (Sys_FileWrite(handle, "MTrk", 4) != 4) return;
+			if (write32bit(handle, tlen) < 0) return;
+			if (Sys_FileWrite(handle, mididata.track[i].data, tlen) != tlen) return;
+		}
+
+		if (Sys_FileClose(handle) < 0) return;
+
+		FreeTracks();	//	Possibly not needed
+#else
+		void* mid = Z_Malloc(256 * 1024);
+		res = qmus2mid((char*)Mus_SndPtr, (char*)mid, len);
+		if (!res) return;
+
+		if ((handle = Sys_FileOpenWrite("vv_temp.mid")) < 0) return;
+		if (Sys_FileWrite(handle, mid, res) != res) return;
+		if (Sys_FileClose(handle) < 0) return;
+
+		FreeTracks();	//	Possibly not needed
+#endif
 	}
 	else
 	{
@@ -363,38 +404,13 @@ static void StartMidiSong(char* song, boolean loop)
 		return;
 	}
 
-	if ((handle = Sys_FileOpenWrite("vv_temp.mid")) < 0) return;
-
-	memcpy(hdr.ID, MIDIMAGIC, 4);
-	hdr.hdr_size = BigLong(6);
-	hdr.num_tracks = BigShort((word)TrackCnt);
-	hdr.divisions = BigShort((word)mididata.divisions);
-	if (TrackCnt > 1) hdr.type = BigShort(1);
-	else hdr.type = 0;
-
-	if (Sys_FileWrite(handle, &hdr, sizeof(hdr)) != sizeof(hdr)) return;
-
-
-	for (i = 0; i < TrackCnt; i++)
-	{
-		dword tlen = mididata.track[i].len;
-		if (Sys_FileWrite(handle, "MTrk", 4) != 4) return;
-		if (write32bit(handle, tlen) < 0) return;
-		if (Sys_FileWrite(handle, mididata.track[i].data, tlen) != tlen) return;
-	}
-
-	if (Sys_FileClose(handle) < 0) return;
-
-
-	FreeTracks();	//	Possibly not needed
-
 	music = Mix_LoadMUS("vv_temp.mid");
 
 	if (!music)
 	{
-    		FreeLump();
+		FreeLump();
 		FreeTracks();
-    		return;
+		return;
 	}
 
 	Mix_FadeInMusic(music, loop, 2000);
@@ -473,6 +489,7 @@ static void StopMidiSong(void)
 //
 //**************************************************************************
 
+#if 0
 //==========================================================================
 //
 //  FirstChannelAvailable
@@ -860,13 +877,395 @@ static void FreeTracks(void)
 		}
 	}
 }
+#else
+//==========================================================================
+//
+//  FirstChannelAvailable
+//
+//==========================================================================
+
+static int FirstChannelAvailable(void)
+{
+	int 	i;
+	int 	old15 = MUS2MIDchannel[15];
+	int		max = -1;
+
+	MUS2MIDchannel[15] = -1;
+	for (i = 0; i < 16; i++)
+    	if (MUS2MIDchannel[i] > max)
+    		max = MUS2MIDchannel[i];
+	MUS2MIDchannel[15] = old15 ;
+
+	return (max == 8 ? 10 : max + 1);
+}
+
+//==========================================================================
+//
+//  TWriteByte
+//
+//==========================================================================
+
+static void TWriteByte(int MIDItrack, char data)
+{
+	if (tracks[MIDItrack].current < TRACKBUFFERSIZE)
+    {
+    	tracks[MIDItrack].data[tracks[MIDItrack].current] = data;
+	}
+	else
+    {
+		Sys_Error("qmus2mid: Track buffer full.");
+    }
+	tracks[MIDItrack].current++;
+}
+
+//==========================================================================
+//
+//	TWriteBuf
+//
+//==========================================================================
+
+static void TWriteBuf(int MIDItrack, byte *buf, int size)
+{
+	int		i;
+
+	for (i = 0; i < size; i++)
+    {
+    	TWriteByte(MIDItrack, buf[i]);
+    }
+}
+
+//==========================================================================
+//
+//  TWriteVarLen
+//
+//==========================================================================
+
+static void TWriteVarLen(int tracknum, register dword value)
+{
+	register dword	buffer ;
+
+	buffer = value & 0x7f ;
+	while( (value >>= 7) )
+    {
+      	buffer <<= 8 ;
+      	buffer |= 0x80 ;
+      	buffer += (value & 0x7f) ;
+    }
+  	while (1)
+    {
+		TWriteByte(tracknum, buffer);
+      	if (buffer & 0x80)
+			buffer >>= 8 ;
+      	else
+			break;
+    }
+}
+
+//==========================================================================
+//
+//  ReadTime
+//
+//==========================================================================
+
+static dword ReadTime(char **file)
+{
+	register dword 	time = 0 ;
+	int 			data;
+
+	do
+    {
+    	data = *(*file)++;
+		if (data != EOF)
+			time = (time << 7) + (data & 0x7F);
+    } while ((data != EOF) && (data & 0x80));
+
+	return time;
+}
+
+//==========================================================================
+//
+//  convert
+//
+//==========================================================================
+
+static boolean convert(char *mus, int length)
+{
+	char				*mus_ptr;
+	byte				et;
+	int					MUSchannel;
+	int					MIDIchannel;
+	int					MIDItrack = 0;
+	int					NewEvent;
+	int 				i;
+	int					event;
+	int					data;
+	dword				DeltaTime;
+	dword				TotalTime = 0;
+	byte				MUS2MIDcontrol[15] =
+	{
+	    0,				/* Program change - not a MIDI control change */
+	    0x00,			/* Bank select */
+	    0x01,			/* Modulation pot */
+	    0x07,			/* Volume */
+	    0x0A,			/* Pan pot */
+	    0x0B,			/* Expression pot */
+	    0x5B,			/* Reverb depth */
+	    0x5D,			/* Chorus depth */
+	    0x40,			/* Sustain pedal */
+	    0x43,			/* Soft pedal */
+	    0x78,			/* All sounds off */
+	    0x7B,			/* All notes off */
+	    0x7E,			/* Mono */
+	    0x7F,			/* Poly */
+	    0x79			/* Reset all controllers */
+	};
+	byte				MIDIchan2track[16];
+  	char 				ouch = 0;
+	MUSheader			*MUSh;
+
+	for (i = 0 ; i < 16 ; i++)
+	{
+		MUS2MIDchannel[i] = -1;
+	}
+	for (i = 0 ; i < 32 ; i++)
+	{
+      	tracks[i].current = 0;
+      	tracks[i].vel = 64;
+      	tracks[i].DeltaTime = 0;
+      	tracks[i].LastEvent = 0;
+      	tracks[i].data = NULL;
+	}
+
+	MUSh = (MUSheader *)mus;
+	if (strncmp(MUSh->ID, MUSMAGIC, 4))
+    {
+    	GCon->Log("Not a MUS file");
+		return false;
+	}
+
+	if (MUSh->channels > 15)	 /* <=> MUSchannels+drums > 16 */
+    {
+    	GCon->Log(NAME_Dev,"Too meny channels");
+		return false;
+	}
+
+	mus_ptr = mus + MUSh->ScoreStart;
+
+	tracks[0].data = (byte*)Z_Malloc(TRACKBUFFERSIZE, PU_MUSIC, 0);
+	TWriteBuf(0, midikey, 6);
+	TWriteBuf(0, miditempo, 7);
+
+	TrackCnt = 1;	//	Music starts here
+
+	event = *(mus_ptr++);
+	et = event_type(event);
+	MUSchannel = channel(event);
+	while ((et != 6) && mus_ptr - mus < length && (event != EOF))
+    {
+		if (MUS2MIDchannel[MUSchannel] == -1)
+		{
+	  		MIDIchannel = MUS2MIDchannel[MUSchannel] =
+	    		(MUSchannel == 15 ? 9 : FirstChannelAvailable());
+	  		MIDItrack   = MIDIchan2track[MIDIchannel] = TrackCnt++;
+			tracks[MIDItrack].data = (byte*)Z_Malloc(TRACKBUFFERSIZE, PU_MUSIC, 0);
+		}
+      	else
+		{
+	  		MIDIchannel = MUS2MIDchannel[MUSchannel];
+	  		MIDItrack   = MIDIchan2track[MIDIchannel];
+		}
+      	TWriteVarLen(MIDItrack, tracks[MIDItrack].DeltaTime);
+      	tracks[MIDItrack].DeltaTime = 0 ;
+      	switch (et)
+		{
+		 case 0:		/* release note */
+  			NewEvent = 0x90 | MIDIchannel;
+   			TWriteByte(MIDItrack, NewEvent);
+   			tracks[MIDItrack].LastEvent = NewEvent;
+  			data = *(mus_ptr++);
+  			TWriteByte(MIDItrack, data);
+  			TWriteByte(MIDItrack, 0);
+  			break;
+		 case 1:
+  			NewEvent = 0x90 | MIDIchannel;
+   			TWriteByte(MIDItrack, NewEvent);
+   			tracks[MIDItrack].LastEvent = NewEvent;
+  			data = *(mus_ptr++);
+  			TWriteByte(MIDItrack, data & 0x7F);
+  			if (data & 0x80)
+    			tracks[MIDItrack].vel = *(mus_ptr++);
+  			TWriteByte(MIDItrack, tracks[MIDItrack].vel);
+  			break;
+		 case 2:
+  			NewEvent = 0xE0 | MIDIchannel;
+   			TWriteByte(MIDItrack, NewEvent);
+   			tracks[MIDItrack].LastEvent = NewEvent;
+  			data = *(mus_ptr++);
+  			TWriteByte(MIDItrack, (data & 1) << 6);
+  			TWriteByte(MIDItrack, data >> 1);
+  			break;
+		 case 3:
+  			NewEvent = 0xB0 | MIDIchannel;
+   			TWriteByte(MIDItrack, NewEvent);
+   			tracks[MIDItrack].LastEvent = NewEvent;
+  			data = *(mus_ptr++) ;
+  			TWriteByte(MIDItrack, MUS2MIDcontrol[data]);
+  			if (data == 12)
+    			TWriteByte(MIDItrack, MUSh->channels + 1);
+  			else
+    			TWriteByte(MIDItrack, 0);
+  			break;
+		 case 4:
+  			data = *(mus_ptr++);
+  			if (data)
+    		{
+      			NewEvent = 0xB0 | MIDIchannel;
+  				TWriteByte(MIDItrack, NewEvent);
+  				tracks[MIDItrack].LastEvent = NewEvent;
+      			TWriteByte(MIDItrack, MUS2MIDcontrol[data]);
+    		}
+  			else
+    		{
+      			NewEvent = 0xC0 | MIDIchannel;
+  				TWriteByte(MIDItrack, NewEvent);
+  				tracks[MIDItrack].LastEvent = NewEvent;
+    		}
+  			data = *(mus_ptr++);
+  			TWriteByte(MIDItrack, data);
+  			break;
+		 case 5:
+		 case 7:
+			GCon->Log(NAME_Dev,"MUS file corupted");
+  			return false;
+		 default:
+           	break;
+		}
+      	if (last(event))
+		{
+	  		DeltaTime = ReadTime(&mus_ptr);
+	  		TotalTime += DeltaTime;
+	  		for (i = 0; i < (int)TrackCnt; i++)
+	    		tracks[i].DeltaTime += DeltaTime;
+		}
+      	event = *(mus_ptr++);
+      	if (event != EOF)
+      	{
+	  		et = event_type(event);
+	  		MUSchannel = channel(event);
+		}
+      	else
+        {
+			ouch = 1;
+		}
+	}
+
+    for (i=0; i<TrackCnt; i++)
+    {
+		TWriteBuf(i, track_end, 4);
+	}
+
+	if (ouch)
+    {
+		GCon->Logf(NAME_Dev, "WARNING : There are bytes missing at the end.");
+		GCon->Logf(NAME_Dev, "The end of the MIDI file might not fit the original one.");
+	}
+
+	return true;
+}
+
+//==========================================================================
+//
+//  WriteBuf
+//
+//==========================================================================
+
+static void WriteBuf(void *p, int size)
+{
+	memcpy(mid_file, p, size);
+	mid_file += size;
+}
+
+//==========================================================================
+//
+//	WriteMIDIFile
+//
+//==========================================================================
+
+static void WriteMIDIFile(void)
+{
+	int				i;
+	dword			size;
+	MIDheader*		hdr;
+
+	//	Header
+	hdr = (MIDheader*)mid_file;
+	memcpy(hdr->ID, MIDIMAGIC, 4);
+    hdr->hdr_size   = BigLong(6);
+    hdr->type       = BigShort(1);
+	hdr->num_tracks = BigShort(TrackCnt);
+	hdr->divisions  = BigShort(89);
+	mid_file += sizeof(*hdr);
+
+	//	Tracks
+	for (i = 0; i < (int)TrackCnt; i++)
+    {
+		size = BigLong(tracks[i].current);
+		WriteBuf((void*)"MTrk", 4);
+		WriteBuf(&size, 4);
+		WriteBuf(tracks[i].data, tracks[i].current);
+	}
+}
+
+//==========================================================================
+//
+//  FreeTracks
+//
+//==========================================================================
+
+static void FreeTracks(void)
+{
+	int i ;
+
+	for (i = 0; i < 16; i++)
+    {
+    	if (tracks[i].data)
+        {
+      		Z_Free(tracks[i].data);
+            tracks[i].data = NULL;
+		}
+	}
+}
+
+//==========================================================================
+//
+//  qmus2mid
+//
+//==========================================================================
+
+int qmus2mid(char *mus, char *mid, int length)
+{
+	mid_file = mid;
+
+	if (convert(mus, length))
+    {
+	    WriteMIDIFile();
+	}
+	FreeTracks();
+
+    return mid_file - mid;
+}
+
+#endif
 
 //**************************************************************************
 //
 //	$Log$
+//	Revision 1.6  2005/05/26 17:00:46  dj_jl
+//	Working MUS to MID conversion
+//
 //	Revision 1.5  2005/04/28 07:16:15  dj_jl
 //	Fixed some warnings, other minor fixes.
-//
+//	
 //	Revision 1.4  2004/10/11 06:49:57  dj_jl
 //	SDL patches.
 //	
