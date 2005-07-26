065e4d3a9d0c5fb78ff20005b674cb409b3477f5
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2005-07-26 18:54:25 +0000

    Removed latvian version.

--- shotstitle_l.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=windows-1257" />
-	<link rel="stylesheet" href="styles.css" type="text/css" />
-</head>
-<body>
-<h1>Skrīnšoti</h1>
-</body>
-</html>
