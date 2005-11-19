83403b6d8e66b1701805c940fcd9abf15d18cb7c
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2005-11-19 15:36:03 +0000

    Fixed compiler problem

--- utils/glbsp/wad.c
+++ utils/glbsp/wad.c
@@ -1633,12 +1633,12 @@ void ReportV5Switches(void)
 {
   lump_t *cur;
 
+  int saw_zdbsp = FALSE;
+
   PrintMsg(
     "V5 FORMAT UPGRADES.  The following levels require a Doom port\n"
     "which supports V5 GL-Nodes, otherwise they will fail (or crash).\n\n"
   );
-
-  int saw_zdbsp = FALSE;
 
   for (cur=wad.dir_head; cur; cur=cur->next)
   {
