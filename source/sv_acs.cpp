3cc7369a208bda0af726a47b1304b6ccaade0610
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2005-12-22 19:05:27 +0000

    Added extra licence.

--- source/sv_acs.cpp
+++ source/sv_acs.cpp
@@ -22,6 +22,34 @@
 //**  GNU General Public License for more details.
 //**
 //**************************************************************************
+//**
+//**	This file includes code from ZDoom, copyright 1998-2004 Randy Heit,
+//**  all rights reserved, with the following licence:
+//**
+//** Redistribution and use in source and binary forms, with or without
+//** modification, are permitted provided that the following conditions
+//** are met:
+//**
+//** 1. Redistributions of source code must retain the above copyright
+//**    notice, this list of conditions and the following disclaimer.
+//** 2. Redistributions in binary form must reproduce the above copyright
+//**    notice, this list of conditions and the following disclaimer in the
+//**    documentation and/or other materials provided with the distribution.
+//** 3. The name of the author may not be used to endorse or promote products
+//**    derived from this software without specific prior written permission.
+//**
+//** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+//** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+//** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+//** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//**
+//**************************************************************************
 
 // HEADER FILES ------------------------------------------------------------
 
@@ -4646,9 +4674,12 @@ static void strbin(char *str)
 //**************************************************************************
 //
 //	$Log$
+//	Revision 1.36  2005/12/22 19:05:27  dj_jl
+//	Added extra licence.
+//
 //	Revision 1.35  2005/11/24 20:09:23  dj_jl
 //	Removed unused fields from Object class.
-//
+//	
 //	Revision 1.34  2005/05/26 16:53:59  dj_jl
 //	Created texture manager class
 //	
