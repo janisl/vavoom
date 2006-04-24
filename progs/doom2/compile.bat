9075b3f08ae78e42fd2d4160c6b3d8a06f90c9c3
Francisco Ortega <firebrand_2001@yahoo.com.mx>
2006-04-24 17:43:04 +0000

    Fixed compiling of common progs

--- progs/doom2/compile.bat
+++ progs/doom2/compile.bat
@@ -1,3 +1,7 @@
-vcc -I../common -P../../basev/common/progs shared.vc shared.dat
-vcc -I../common -P../../basev/common/progs clprogs.vc clprogs.dat
-vcc -I../common -P../../basev/common/progs svprogs.vc svprogs.dat
+vcc -I../common -P../common ../common/common.vc ../common/common.dat
+vcc -I../common -P../common ../common/clcommon.vc ../common/clcommon.dat
+vcc -I../common -P../common ../common/svcommon.vc ../common/svcommon.dat
+
+vcc -I../common -P../common shared.vc shared.dat
+vcc -I../common -P../common clprogs.vc clprogs.dat
+vcc -I../common -P../common svprogs.vc svprogs.dat
