a2e7a757c6220db5d526b41ea96fa66f8aec5f64
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2006-07-18 17:22:10 +0000

    Version 1.21.1.

--- source/build.h
+++ source/build.h
@@ -25,8 +25,8 @@
 
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	21
-#define VERSION_RELEASE	0
-#define VERSION_TEXT	"1.21"
+#define VERSION_RELEASE	1
+#define VERSION_TEXT	"1.21.1"
 
 #if !defined CLIENT && !defined SERVER
 #define CLIENT
