016fc901b7c19118e0540dc873ddad1d3b196198
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2006-10-03 19:22:36 +0000

    Renamed "common" progs package to "engine", moved the rest of the native classes to this package.

--- progs/strife/compile.bat
+++ progs/strife/compile.bat
@@ -1,5 +1,4 @@
-vcc -I../common -P../common ../common/common.vc ../common/common.dat
-vcc -I../common -P../common ../common/clcommon.vc ../common/clcommon.dat
+vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
 vcc -I../common -P../common ../common/svcommon.vc ../common/svcommon.dat
 
 vcc -I../common -P../common strfdefs.vc shared.dat
