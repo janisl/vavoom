e1b1e8312fe094e1a4218b8973e2cf42a27e2fbc
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2006-10-09 22:10:07 +0000

    Moved lighting line specials to linespec package.

--- progs/doom/compile.bat
+++ progs/doom/compile.bat
@@ -1,6 +1,5 @@
 vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
 vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
-vcc -I../common -P../common ../common/server/classes.vc ../common/svcommon.dat
 
 vcc -I../common -P../common doomdefs.vc shared.dat
 vcc -I../common -P../common svprogs.vc svprogs.dat
