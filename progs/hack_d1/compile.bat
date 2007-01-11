ca08621ea369078cd1c42af95af5c041addb2608
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2007-01-11 20:40:27 +0000

    Fixed compile BAT files for dehacked.

--- progs/hack_d1/compile.bat
+++ progs/hack_d1/compile.bat
@@ -1,2 +1,6 @@
-vcc -I../common -I../doom/client clprogs
-vcc -I../common -I../doom/server svprogs
+vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
+vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
+
+vcc -I../common -P../common -I../doom ../doom/doomdefs.vc shared.dat
+vcc -I../common -P../common -I../doom svprogs.vc svprogs.dat
+vcc -I../common -P../common -I../doom clprogs.vc clprogs.dat
