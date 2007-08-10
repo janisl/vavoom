f37a8ad8cd2c074c8822400cbf85c2740cf37313
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2007-08-10 18:04:14 +0000

    Removed empty shared package.

--- progs/heretic/compile.bat
+++ progs/heretic/compile.bat
@@ -1,6 +1,5 @@
 vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
 vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
 
-vcc -I../common -P../common herdefs.vc shared.dat
 vcc -I../common -P../common svprogs.vc svprogs.dat
 vcc -I../common -P../common clprogs.vc clprogs.dat
