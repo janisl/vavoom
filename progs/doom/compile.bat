1a662a2ae8c3540adc2651d659281019922c45b0
Francisco Ortega <firebrand_2001@yahoo.com.mx>
2007-11-09 23:07:45 +0000

    Implemented a more generalized way to make particle effects

--- progs/doom/compile.bat
+++ progs/doom/compile.bat
@@ -1,6 +1,41 @@
 vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
 vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
 
+cd..
+cd common
+copy engine.dat D:\korax\vavoom\source\svn\basev\common\progs\
+copy linespec.dat D:\korax\vavoom\source\svn\basev\common\progs\
+cd..
+cd..
+cd basev
+cd common
+
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\vavoom\basev\common\"
+
+cd..
+cd..
+cd progs
+cd doom
+
 vcc -I../common -P../common doomdefs.vc shared.dat
 vcc -I../common -P../common svprogs.vc svprogs.dat
 vcc -I../common -P../common clprogs.vc clprogs.dat
+
+copy shared.dat D:\korax\vavoom\source\svn\basev\doom1\progs\
+copy svprogs.dat D:\korax\vavoom\source\svn\basev\doom1\progs\
+copy clprogs.dat D:\korax\vavoom\source\svn\basev\doom1\progs\
+
+cd..
+cd..
+cd basev
+cd doom
+
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\vavoom\basev\doom\"
+
+cd..
+cd doom1
+
+vlumpy basepak.ls
+copy basepak.pk3 "D:\Korax\vavoom\basev\doom1\"
