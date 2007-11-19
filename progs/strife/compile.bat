0d5b3452a6c7612f1d651f5a7c3b68b2e53d9097
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2007-11-19 18:53:21 +0000

    Created uibase package.

--- progs/strife/compile.bat
+++ progs/strife/compile.bat
@@ -1,5 +1,6 @@
-vcc -I../common -P../common ../common/engine/classes.vc ../common/engine.dat
-vcc -I../common -P../common ../common/linespec/classes.vc ../common/linespec.dat
+vcc -P../common ../common/engine/classes.vc ../common/engine.dat
+vcc -P../common ../common/linespec/classes.vc ../common/linespec.dat
+vcc -P../common ../common/uibase/classes.vc ../common/uibase.dat
 
 vcc -I../common -P../common svprogs.vc svprogs.dat
 vcc -I../common -P../common clprogs.vc clprogs.dat
