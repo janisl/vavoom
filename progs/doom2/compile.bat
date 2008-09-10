67ee379a597500abd8f6028d9f51633a2e74eef1
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2008-09-10 20:22:53 +0000

    Changed Doom progs to be in source form in PK3 file

--- progs/doom2/compile.bat
+++
@@ -1,6 +0,0 @@
-vcc -P../common ../common/engine/classes.vc ../common/engine.dat
-vcc -P../common ../common/linespec/classes.vc ../common/linespec.dat
-vcc -P../common ../common/uibase/classes.vc ../common/uibase.dat
-
-vcc -I../common -P../common game.vc game.dat
-vcc -I../common -P../common cgame.vc cgame.dat
