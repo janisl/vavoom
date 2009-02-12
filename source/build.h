db12049ac4782ee6753c4148907a6e7452e00ee9
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2009-02-12 19:22:03 +0000

    Increased version number

--- source/build.h
+++ source/build.h
@@ -24,9 +24,9 @@
 //**************************************************************************
 
 #define VERSION_MAJOR	1
-#define VERSION_MINOR	29
+#define VERSION_MINOR	30
 #define VERSION_RELEASE	0
-#define VERSION_TEXT	"1.29"
+#define VERSION_TEXT	"1.30"
 
 #if !defined CLIENT && !defined SERVER
 #define CLIENT
--- source/progdefs.h
+++ source/progdefs.h
@@ -26,7 +26,7 @@
 #ifndef OPCODE_INFO
 
 #define PROG_MAGIC		"VPRG"
-#define PROG_VERSION	29
+#define PROG_VERSION	30
 
 enum
 {
