e8ae85bc8d7e0ca149e533c26ea57949ce749385
Francisco Ortega <firebrand_2001@yahoo.com.mx>
2011-05-08 15:21:20 +0000

    Applied a patch file provided by inode64 that fixes more compilation errors of GME library under Linux.

--- source/gme/src/blargg_common.h
+++ source/gme/src/blargg_common.h
@@ -124,8 +124,13 @@ global scope already. */
 		typedef unsigned short   uint16_t;
 		typedef int               int32_t;
 		typedef unsigned int     uint32_t;
-		typedef __int64           int64_t;
+#ifdef _MSC_VER 
+	    typedef __int64           int64_t;
 		typedef unsigned __int64 uint64_t;
+#else
+        typedef long long int int64_t;
+        typedef unsigned long long int uint64_t;
+#endif 
 	};
 #endif
 
